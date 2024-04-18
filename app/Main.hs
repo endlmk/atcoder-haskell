bacb47658359cabe9a82223084ecd185ce3e13c3
endlmk <1269467+endlmk@users.noreply.github.com>
2024-04-18 22:45:31 +0900

    ABC087B

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,17 @@
 module Main where
 
-f x = if even x then 1 + f (x `div` 2) else 0
-
 ints :: IO [Int]
 ints = map read . words <$> getLine
 
 main :: IO ()
 main = do
-  n <- getLine
-  a <- ints
-  print $ minimum $ map f a
+  a <- getLine
+  let anum = read a :: Integer
+  b <- getLine
+  let bnum = read b :: Integer
+  c <- getLine
+  let cnum = read c :: Integer
+  x <- getLine
+  let xnum = read x :: Integer
+  let pairs = [(na, nb, nc) | na <- [0 .. anum], nb <- [0 .. bnum], nc <- [0 .. cnum]]
+  print (sum (map (\(a1, b1, c1) -> if (500 * a1 + 100 * b1 + 50 * c1) == xnum then 1 else 0) pairs))
