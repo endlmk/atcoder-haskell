be6259c7761d8ff0a6a457479a2453e646fbd019
endlmk <1269467+endlmk@users.noreply.github.com>
2024-04-21 21:51:09 +0900

    ABC085B

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,13 @@
 module Main where
 
-import Data.List (sortBy, unfoldr)
+import Control.Monad (replicateM)
+import Data.List (nub)
 
 ints :: IO [Int]
 ints = map read . words <$> getLine
 
 main :: IO ()
 main = do
-  _ <- getLine
-  a <- ints
-  let ordered = sortBy (flip compare) a
-  let alice = sum [x | (x, i) <- zip ordered [1 ..], odd i]
-  let bob = sum [x | (x, i) <- zip ordered [1 ..], even i]
-  print (alice - bob)
+  n <- readLn
+  d <- replicateM n getLine
+  print (length (nub d))
