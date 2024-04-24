79108ea19e1c051720000205bbff5eae9302fc7a
endlmk <1269467+endlmk@users.noreply.github.com>
2024-04-24 22:32:13 +0900

    ABC086C

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,21 @@
 module Main where
 
-import Data.List (stripPrefix)
-import Data.Maybe (listToMaybe, mapMaybe)
+import Control.Monad (replicateM)
 
 ints :: IO [Int]
 ints = map read . words <$> getLine
 
-solve :: String -> Bool
-solve [] = True
-solve s = maybe False solve (listToMaybe (mapMaybe (`stripPrefix` s) revwords))
+diff :: [[Int]] -> Bool
+diff [_] = True
+diff ([t0, x0, y0] : xs) =
+  rem >= 0 && even rem && diff xs
   where
-    revwords = map reverse ["dream", "dreamer", "erase", "eraser"]
+    rem = ((t1 - t0) - (abs (x1 - x0) + abs (y1 - y0)))
+    [t1, x1, y1] = head xs
 
 main :: IO ()
 main = do
-  s <- getLine
-  putStrLn (if solve (reverse s) then "YES" else "NO")
+  n <- readLn
+  p <- replicateM n ints
+  let t = [0, 0, 0] : p
+  putStrLn (if diff t then "Yes" else "No")
